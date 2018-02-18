a3006f41390307c49cdd9633477c5acb2b157b55
TronFortyTwo <emanuele.sorce@hotmail.com>
2018-02-18 12:19:17 +0100

    Fixes

--- install-dep.sh
+++ install-dep.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt update
-apt install cmake build-essential click qtdeclarative5-dev intltool
+apt install cmake build-essential click qtdeclarative5-dev intltool -y
--- scripts/install.sh
+++ scripts/install.sh
@@ -4,4 +4,4 @@ mkdir -p ~/.cache/ontheroad.emanuelesorce/bin/
 
 cp /opt/click.ubuntu.com/ontheroad.emanuelesorce/current/scripts/otr ~/.cache/ontheroad.emanuelesorce/bin/
 
-echo "export PATH=$PATH:~/.cache/ontheroad.emanuelesorce/bin" >> ~/.bashrc
+echo "export PATH=\$PATH:~/.cache/ontheroad.emanuelesorce/bin" >> ~/.bashrc
